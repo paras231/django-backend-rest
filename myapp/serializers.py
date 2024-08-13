5da8e8220a71cc8d56040632ecbfd992f54facad
paras231 <sharmaparas553@gmail.com>
2024-08-14 00:01:30 +0530

    updated custom user model , serializer and user creation apis , need to fix the login gunctinality

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,7 +1,7 @@
 
 
 from rest_framework import serializers
-from .models import Post,Author,Like
+from .models import Post,Author,Like,CustomUser
 
 
 class AuthorSerializer(serializers.ModelSerializer):
@@ -20,3 +20,19 @@ class LikeSerializer(serializers.ModelSerializer):
       class Meta:
           model = Like
           fields = ['user', 'post', 'created_at']
+
+
+class UserSerializer(serializers.ModelSerializer):
+      class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'first_name', 'last_name', 'password', 'is_staff', 'is_active']
+        extra_kwargs = {'password': {'write_only': True}}
+
+        def create(self, validated_data):
+            user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+            return user
